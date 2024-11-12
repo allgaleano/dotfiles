c7dc55af44a989b47e58dff383c4ebfa5c5aa72b
allgaleano <all.galeano.diaz@gmail.com>
2024-11-12 15:40:19 +0100

    fix: polybar dual monitor and wait till previous polybar shutdowns completely before running it again on restarts

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
-# polybar-msg cmd quit
-# Otherwise you can use the nuclear option:
 killall -q polybar
 
-# Launch bar1 and bar2
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch main bar
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
 polybar laptop 2>&1 | tee -a /tmp/polybar1.log & disown
-polybar top_monitor 2>&1 | tee -a /tmp/polybar1.log & disown
+
+# Check if HDMI is connected and launch second bar if it is
+if xrandr | grep "HDMI-A-1-0 connected"; then
+    polybar top_monitor 2>&1 | tee -a /tmp/polybar2.log & disown
+fi
 
 echo "Bars launched..."
