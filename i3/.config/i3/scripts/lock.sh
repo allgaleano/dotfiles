404c03b68865aded7aa303a191f6fd0b4fe4c988
allgaleano <all.galeano.diaz@gmail.com>
2024-11-18 16:31:20 +0100

    minor changes

--- i3/.config/i3/scripts/lock.sh
+++ i3/.config/i3/scripts/lock.sh
@@ -8,16 +8,13 @@ xidlehook \
   --not-when-fullscreen \
   `# Don't lock when there's audio playing` \
   --not-when-audio \
-  `# Dim the screen after 10 minutes, undim if user becomes active` \
   --timer 600 \
-    'brightnessctl --save; brightnessctl set 50%' \
-    'dunstctl close; brightnessctl --restore' \
-  `# Undim & warn about suspension after 10 seconds` \
+    'dunstctl close' \
   --timer 1 \
     'notify-send "System will suspend in 10 seconds" -u critical' \
-    'dunstctl close; brightnessctl --restore' \
+    'dunstctl close' \
   `# Suspend the system after 10 seconds` \
   --timer 10 \
     'systemctl suspend' \
-    'dunstctl close; brightnessctl --restore' \
+    'dunstctl close' \
   &
