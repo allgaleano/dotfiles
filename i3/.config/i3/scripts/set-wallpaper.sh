22722cac167d73682aa36f0227cb9f16279070bb
allgaleano <all.galeano.diaz@gmail.com>
2024-11-23 17:55:18 +0100

    polybar transparent blured

--- i3/.config/i3/scripts/set-wallpaper.sh
+++ i3/.config/i3/scripts/set-wallpaper.sh
@@ -1,1 +1,17 @@
-feh --bg-fill ~/Media/Pictures/wallpapers/arch_outline.png
+#!/bin/bash
+
+WALLPAPERS_DIR="$HOME/Media/Pictures/wallpapers"
+
+if [ -z "$WALLPAPER" ]; then
+	echo "Error: No wallpaper specified. Please set WALLPAPER environment variabel"
+	exit 1
+fi
+
+WALLPAPER_PATH="$WALLPAPERS_DIR/$WALLPAPER_NAME"
+
+if [ ! -f "$WALLPAPER_PATH" ]; then
+    echo "Error: Wallpaper $WALLPAPER_NAME not found in $WALLPAPERS_DIR"
+    exit 1
+fi
+
+feh --bg-fill "$WALLPAPER_PATH"
