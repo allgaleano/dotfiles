db9d84f0d7e17bf888f6fae2492ff3fadbf6c906
allgaleano <all.galeano.diaz@gmail.com>
2025-01-12 09:26:30 +0100

    feat: bluetooth battery in polybar

--- polybar/.config/polybar/scripts/bluetooth.sh
+++ polybar/.config/polybar/scripts/bluetooth.sh
@@ -8,7 +8,24 @@ else
         echo "󰂯 ON"
     else
         device_name=$(bluetoothctl info | grep "Name" | cut -d ":" -f2 | xargs)
-        echo "󰂯 $device_name"
+        battery_info=$(bluetoothctl info | grep "Battery Percentage")
+        
+        if [ -n "$battery_info" ]; then
+            percentage=$(echo "$battery_info" | grep -o '[0-9]\+' | tail -1)
+            if [ "$percentage" -ge 95 ]; then
+                battery_icon=""
+            elif [ "$percentage" -ge 75 ]; then
+                battery_icon=""
+            elif [ "$percentage" -ge 50 ]; then
+                battery_icon=""
+            elif [ "$percentage" -ge 25 ]; then
+                battery_icon=""
+            else
+                battery_icon=""
+            fi
+            echo "󰂯 $device_name $battery_icon"
+        else
+            echo "󰂯 $device_name"
+        fi
     fi
 fi
-
